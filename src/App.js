bfb45ca14307955ab5db24d500a916389729121b
Robin Saini <robinsaini440@gmail.com>
2025-03-31 21:49:04 +0530

    Updated portfolio with new changes

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => {
     <ThemeProvider>
       <Router>
         <Navbar />
+        <Home />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
