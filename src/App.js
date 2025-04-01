1d06b3a6bb9a09b5cc9e08f828a08a1dd07ee686
Robin Saini <robinsaini440@gmail.com>
2025-04-01 07:28:56 +0530

    Updated portfolio with new changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext";
 import Navbar from "./components/Navbar";
@@ -12,14 +11,15 @@ import Footer from "./components/Footer";
 const App = () => {
   return (
     <ThemeProvider>
-      <Router>
+      {/* basename add kiya */}
+      <Router basename="/my-portfolio">
         <Navbar />
-        <Home />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/skills" element={<Skills />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<h2 style={{ textAlign: "center" }}>Page Not Found</h2>} />
         </Routes>
         <Footer />
       </Router>
@@ -29,3 +29,33 @@ const App = () => {
 
 export default App;
 
+
+// import React from "react";
+// import "./App.css";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { ThemeProvider } from "./context/ThemeContext";
+// import Navbar from "./components/Navbar";
+// import Home from "./components/Home";
+// import Projects from "./components/Projects";
+// import Skills from "./components/Skills";
+// import Contact from "./components/Contact";
+// import Footer from "./components/Footer";
+
+// const App = () => {
+//   return (
+//     <ThemeProvider>
+//       <Router>
+//         <Navbar />
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/projects" element={<Projects />} />
+//           <Route path="/skills" element={<Skills />} />
+//           <Route path="/contact" element={<Contact />} />
+//         </Routes>
+//         <Footer />
+//       </Router>
+//     </ThemeProvider>
+//   );
+// };
+
+// export default App;
