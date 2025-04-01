af552548572603169a1d9a5884ad50fc76cbf7f6
Robin Saini <robinsaini440@gmail.com>
2025-04-01 11:18:39 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import Footer from "./components/Footer";
 const App = () => {
   return (
     <ThemeProvider>
-      {/* basename add kiya */}
       <Router basename="/my-portfolio">
         <Navbar />
         <Routes>
@@ -28,34 +27,3 @@ const App = () => {
 };
 
 export default App;
-
-
-// import React from "react";
-// import "./App.css";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import { ThemeProvider } from "./context/ThemeContext";
-// import Navbar from "./components/Navbar";
-// import Home from "./components/Home";
-// import Projects from "./components/Projects";
-// import Skills from "./components/Skills";
-// import Contact from "./components/Contact";
-// import Footer from "./components/Footer";
-
-// const App = () => {
-//   return (
-//     <ThemeProvider>
-//       <Router>
-//         <Navbar />
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/projects" element={<Projects />} />
-//           <Route path="/skills" element={<Skills />} />
-//           <Route path="/contact" element={<Contact />} />
-//         </Routes>
-//         <Footer />
-//       </Router>
-//     </ThemeProvider>
-//   );
-// };
-
-// export default App;
